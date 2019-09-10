75d74781893c96e88fdba789237be631703747ca
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-09-10 23:17:15 +0300

    add class bitcoincontroller

---
+++ api.Controllers/BitcoinController.cs
@@ -0,0 +1,22 @@
+﻿using api.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace api.Controllers
+{
+    public class BitcoinController : ApiController
+    {
+        private readonly IBitcoinLoggerService bitcoinLoggerService;
+
+        public BitcoinController(IBitcoinLoggerService bitcoinLoggerService)
+        {
+            this.bitcoinLoggerService = bitcoinLoggerService;
+        }
+
+
+    }
+}
