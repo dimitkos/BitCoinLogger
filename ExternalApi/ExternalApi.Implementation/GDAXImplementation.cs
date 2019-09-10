c78bd6e21b84662553b17474d105e142237110cb
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-09-10 22:38:36 +0300

    Implement method GetDataFromGdax

--- ExternalApi/ExternalApi.Implementation/GDAXImplementation.cs
+++ ExternalApi/ExternalApi.Implementation/GDAXImplementation.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ExternalApi.Interfaces;
+using ExternalApi.Types;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,14 @@ using System.Threading.Tasks;
 
 namespace ExternalApi.Implementation
 {
-    public class GDAXImplementation
+    public class GDAXImplementation : IGDAXService
     {
+        public GdaxResponse GetDataFromGdax()
+        {
+            var client = new RestClient("https://api.gdax.com");
+            var request = new RestRequest("/products/BTC-USD/ticker", Method.GET);
+            var queryResult = client.Get<GdaxResponse>(request).Data;
+            return queryResult;
+        }
     }
 }
