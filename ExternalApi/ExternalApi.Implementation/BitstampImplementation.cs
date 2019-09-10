89de685419a6f6ae1dc4502162832e62d224c026
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-09-10 22:08:27 +0300

    Implement method GetDataFromBitstamp

--- ExternalApi/ExternalApi.Implementation/BitstampImplementation.cs
+++ ExternalApi/ExternalApi.Implementation/BitstampImplementation.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ExternalApi.Interfaces;
+using ExternalApi.Types;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,14 @@ using System.Threading.Tasks;
 
 namespace ExternalApi.Implementation
 {
-    public class BitstampImplementation
+    public class BitstampImplementation : IBitstampService
     {
+        public BitstampResponse GetDataFromBitstamp()
+        {
+            var client = new RestClient("https://www.bitstamp.net");
+            var request = new RestRequest("/api/ticker/", Method.GET);
+            var queryResult = client.Get<BitstampResponse>(request).Data;
+            return queryResult;
+        }
     }
 }
