9f010c5a286b32d9c008e63a2319b4d6a9098570
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-09-10 21:11:07 +0300

    Initialize api and nuget

---
+++ Api/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+using SimpleInjector.Lifestyles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Api
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            var container = new Container();
+
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+            //container.Register<IService, OnCoursesImplementation>(Lifestyle.Scoped);
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+
+            container.Verify();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
