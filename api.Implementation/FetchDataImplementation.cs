b23a09c71af3b534f0a0b47e475e542722bf6be5
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-09-10 23:04:27 +0300

    Implement FetchDataImplementation and the interface

---
+++ api.Implementation/FetchDataImplementation.cs
@@ -0,0 +1,33 @@
+﻿using api.Interfaces;
+using ExternalApi.Interfaces;
+using ExternalApi.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Implementation
+{
+    public class FetchDataImplementation : IBitcoinLoggerService
+    {
+        private readonly IBitstampService bitstampService;
+        private readonly IGDAXService gdaxService;
+
+        public FetchDataImplementation(IBitstampService bitstampService, IGDAXService gdaxService)
+        {
+            this.bitstampService = bitstampService;
+            this.gdaxService = gdaxService;
+        }
+
+        public BitstampResponse FecthBitstampData()
+        {
+            return bitstampService.GetDataFromBitstamp();
+        }
+
+        public GdaxResponse FecthGdaxData()
+        {
+            return gdaxService.GetDataFromGdax();
+        }
+    }
+}
---
+++ api.Interfaces/IBitcoinLoggerService.cs
@@ -0,0 +1,16 @@
+﻿using ExternalApi.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Interfaces
+{
+    public interface IBitcoinLoggerService
+    {
+        BitstampResponse FecthBitstampData();
+
+        GdaxResponse FecthGdaxData();
+    }
+}
